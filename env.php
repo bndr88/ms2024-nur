f7f69ee2f3b2c7b463569bab73a5c4aadfde67e8
Wender Mendez <wendermendez@gmail.com>
2025-03-20 13:46:39 -0400

    Organización y primer intento docker-compose
    Organización y primer intento docker-compose

--- env.php
+++ env.php
@@ -2,12 +2,27 @@
 
 namespace Mod2Nur;
 
-//require __DIR__ . '/../../vendor/autoload.php';
 require __DIR__ . '/vendor/autoload.php';
 
 use Illuminate\Database\Capsule\Manager as Capsule;
 
+/*function env($key, $default = null) {
+    return getenv($key) ?: $default;
+}*/
+
 $capsule = new Capsule();
+$capsule->addConnection([
+    'driver'    => env('DB_CONNECTION', 'mysql'),
+    'host'      => env('DB_HOST', '127.0.0.1'),
+    'database'  => env('DB_DATABASE', 'nutrinur'), 
+    'username'  => env('DB_USERNAME', 'root'),
+    'password'  => env('DB_PASSWORD', 'Lm12345'), 
+    'charset'   => 'utf8',
+    'collation' => 'utf8_unicode_ci',
+    'prefix'    => '',
+], 'default');
+
+/*$capsule = new Capsule();
 $capsule->addConnection([
     'driver'    => 'mysql',
     'host'      => '127.0.0.1',
@@ -18,7 +33,7 @@ $capsule->addConnection([
     'collation' => 'utf8_unicode_ci',
     'prefix'    => '',
 ], 'default');
-
+*/
 $capsule->setAsGlobal();
 $capsule->bootEloquent();
 
