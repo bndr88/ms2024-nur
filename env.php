52c70b7671594765f67f63e8a52ed77ff0073ae1
Wender Mendez <wendermendez@gmail.com>
2025-04-11 01:42:26 -0400

    Prueba Pre-commit

--- env.php
+++ env.php
@@ -8,12 +8,15 @@ use Illuminate\Database\Capsule\Manager as Capsule;
 
 $capsule = new Capsule();
 $capsule->addConnection([
-    'driver' => env('DB_CONNECTION', 'mysql'),
-    'host' => env('DB_HOST', '127.0.0.1'),
-    'database' => env('DB_DATABASE', 'nutrinur2'),
-    'username' => env('DB_USERNAME', 'root'),'password' => env('DB_PASSWORD', 'Lm12345'),
-    'charset' => 'utf8',
-    'collation' => 'utf8_unicode_ci',    'prefix' => '',], 'default');
+	'driver' => env('DB_CONNECTION', 'mysql'),
+	'host' => env('DB_HOST', '127.0.0.1'),
+	'database' => env('DB_DATABASE', 'nutrinur2'),
+	'username' => env('DB_USERNAME', 'root'),
+	'password' => env('DB_PASSWORD', 'Lm12345'),
+	'charset' => 'utf8',
+	'collation' => 'utf8_unicode_ci',
+	'prefix' => '',
+], 'default');
 
 $capsule->setAsGlobal();
 $capsule->bootEloquent();
