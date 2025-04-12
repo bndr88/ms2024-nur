922ffb9a0198d4e7661a5b04505232d3ff7e120d
Wender Mendez <wendermendez@gmail.com>
2025-04-12 00:37:45 -0400

    actualizacion

--- env.php
+++ env.php
@@ -11,9 +11,9 @@ $capsule->addConnection([
 	'driver' => env('DB_CONNECTION', 'mysql'),
 	'host' => env('DB_HOST', '127.0.0.1'),
 	'database' => env('DB_DATABASE', 'nutrinur2'),
-'username' => env('DB_USERNAME', 'root'),
-'password' => env('DB_PASSWORD', 'Lm12345'),
-	'charset' => 'utf8'  ,
+	'username' => env('DB_USERNAME', 'root'),
+	'password' => env('DB_PASSWORD', 'Lm12345'),
+	'charset' => 'utf8',
 	'collation' => 'utf8_unicode_ci',
 	'prefix' => '',
 ], 'default');
